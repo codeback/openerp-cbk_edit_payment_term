da4bc7166a22336c9b635a1f433599cb6e25d2c9
root <root@codeback.es>
2013-10-09 18:56:16 +0200

    Solucionado error en init

--- __init__.py
+++ __init__.py
@@ -21,6 +21,6 @@
 #
 ##############################################################################
 
-import partner
+import account_invoice
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
